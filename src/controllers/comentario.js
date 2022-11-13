e1cd4ebd9d7c2262f73ccb9367ea0ba039a40afa
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2022-11-13 10:44:06 -0400

    update

---
+++ src/controllers/comentario.js
@@ -0,0 +1,56 @@
+//rutas database
+const createcomentario=require('../database')
+const editcomentario=require('../database')
+const searchidcomentario=require('../database')
+const searchusernamecomentario=require('../database')
+const comentario={}
+
+
+
+//create comentario
+comentario.create=(req,res)=>{
+    try{
+        createcomentario.createcomentario(req,res);
+        res.send('create comentario')
+    }catch(e){
+        console.log(e)
+    }  
+
+}
+//like
+
+//edit comentario
+comentario.edit=(req,res)=>{
+try {
+    editcomentario.editcomentario(req,res);
+    res.send('edit comentario')
+}catch(e){
+    console.log(e)
+}
+
+}
+
+
+//search comentario id
+comentario.searchid=(req,res)=> {
+
+    try{
+        searchidcomentario.searchidcomentario(req,res);
+        res.send('search comentario')
+    }catch(e){
+        console.log(e)
+    }
+}
+//search usernamecomentario
+comentario.searchusernamecomentario=(req,res)=> {
+    try{
+        searchusernamecomentario.searchusernamecomentario(req,res);
+        res.send('search comentario for username')
+    }catch(e){
+        console.log(e)
+    }
+
+}
+
+
+module.exports= comentario
